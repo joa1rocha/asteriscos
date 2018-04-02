7487c842b77c42e6938f993ae8a6acafb287e8c7
joa1rocha <joa1.rocha@gmail.com>
2018-04-02 12:31:35 +0100

    Small teaks and added some AWS settings

--- wp-content/themes/asteriscos/functions.php
+++ wp-content/themes/asteriscos/functions.php
@@ -6,7 +6,6 @@
  *
  * @package WordPress
  * @subpackage Asteriscos
- * @since 1.0
  */
 
 /**
@@ -22,34 +21,6 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
 	return;
 }
 
-add_theme_support( 'custom-header' );
-function themename_custom_header_setup() {
-	$defaults = array(
-		// Default Header Image to display
-		'default-image' => get_template_directory_uri() . '/asteriscos/wp-content/uploads/2018/02/Prancheta-7-8.png',
-		// Display the header text along with the image
-		'header-text' => true,
-		// Header text color default
-//		'default-text-color' => '000',
-		// Header image width (in pixels)
-//		'width' => 1000,
-		// Header image height (in pixels)
-//		'height' => 198,
-		// Header image random rotation default
-//		'random-default' => false,
-		// Enable upload of image file in admin
-//		'uploads' => false,
-		// function to be called in theme head section
-//		'wp-head-callback' => 'wphead_cb',
-		//  function to be called in preview page head section
-//		'admin-head-callback' => 'adminhead_cb',
-		// function to produce preview markup in the admin screen
-//		'admin-preview-callback' => 'adminpreview_cb',
-	);
-}
-add_action( 'after_setup_theme', 'themename_custom_header_setup' );
-
-
 function add_theme_scripts() {
     enqueue_styles();
     enqueue_scripts();
@@ -57,7 +28,6 @@ function add_theme_scripts() {
 
 function enqueue_styles() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css', array());
 	wp_enqueue_style( 'asteriscos', get_template_directory_uri() . '/assets/css/asteriscos.css', array());
 	wp_enqueue_style( 'noscript', get_template_directory_uri() . '/assets/css/noscript.css', array());
@@ -76,7 +46,7 @@ function enqueue_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
-function mytheme_add_woocommerce_support() {
+function asteriscos_add_woocommerce_support() {
 	add_theme_support( 'woocommerce', array(
 		'thumbnail_image_width' => 150,
 		'single_image_width'    => 300,
@@ -91,4 +61,31 @@ function mytheme_add_woocommerce_support() {
 //		),
 	) );
 }
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_action( 'after_setup_theme', 'asteriscos_add_woocommerce_support' );
+
+add_theme_support( 'custom-header' );
+function asteriscos_custom_header_setup() {
+	$defaults = array(
+		// Default Header Image to display
+		'default-image' => get_template_directory_uri() . '/asteriscos/wp-content/uploads/2018/02/Prancheta-7-8.png',
+		// Display the header text along with the image
+		'header-text' => true,
+		// Header text color default
+		'default-text-color' => '000',
+		// Header image width (in pixels)
+//		'width' => 1000,
+		// Header image height (in pixels)
+//		'height' => 198,
+		// Header image random rotation default
+//		'random-default' => false,
+		// Enable upload of image file in admin
+//		'uploads' => false,
+		// function to be called in theme head section
+//		'wp-head-callback' => 'wphead_cb',
+		//  function to be called in preview page head section
+//		'admin-head-callback' => 'adminhead_cb',
+		// function to produce preview markup in the admin screen
+//		'admin-preview-callback' => 'adminpreview_cb',
+	);
+}
+add_action( 'after_setup_theme', 'asteriscos_custom_header_setup' );
